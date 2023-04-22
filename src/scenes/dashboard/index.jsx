fd93b53cac8ce56d3638180384ebba3ae0f87b12
saira512dev <512saira@gmail.com>
2023-04-22 22:48:57 +0400

    dashboard edited

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -107,7 +107,7 @@ function Dashboard() {
             p="1rem" 
             borderRadius="0.55rem"
           >
-            <OverviewChart view="sales" isDashboard={true} />
+            {/* <OverviewChart view="sales" isDashboard={true} /> */}
           </Box>
         <StatBox 
           title="Monthly Sales"
@@ -171,7 +171,7 @@ function Dashboard() {
                 <Typography variant="h6" sx={{ color: theme.palette.secondary[100]}}>
                   Sales By Category
                 </Typography>
-                <BreakdownChart isDashboard={true} />
+                {/* <BreakdownChart isDashboard={true} /> */}
                 <Typography p="0 0.6rem" fontSize="0.8rem" sx={{ color: theme.palette.secondary[200]}} >
                   Breakdown of real states and information via category for revenue made for this year and total sales
                 </Typography>
